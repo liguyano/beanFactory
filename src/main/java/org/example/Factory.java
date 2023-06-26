66f87f5859553a12bbb0f65c02954834369df5ea
liguyano <kurumiwu@foxmail.com>
2023-06-26 19:32:08 +0800

    run success

---
+++ src/main/java/org/example/Factory.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Factory {
+    public int next=1;
+    Factory(int i)
+    {
+        this.next=i;
+        System.out.println("build Factory");
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,18 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
+    public static void f(BeanFactory factory)
+    {
+        Factory b=(Factory) factory.getBean("test");
+        System.out.println(b.next);
+    }
     public static void main(String[] args) {
         BeanFactory factory=new ClassPathXmlApplicationContext("bean.xml");
-
+        Factory a=(Factory) factory.getBean("test");
+        System.out.println(a.getClass());
+        System.out.println(a.next++);
+        f(factory);
         System.out.println("Hello world!");
+
     }
 }
